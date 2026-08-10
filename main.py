4e2c48889e3f3e991a3d181c95d6c9028690448f
kashyapanshu3108-ops <kashyapanshu3106@gmail.com>
2026-08-10 23:42:50 +0530

    add new line for login

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
  # AI STUDY BUDDY CHATBOT
-print("HELLLO! WELCOME TO THE RULE BASED CHATBOT ")
+print("HELLLO! WELCOME TO THE  CHATBOT ")
+print("you can ask me basic question, type 'bye'to exit from the bot")
+
