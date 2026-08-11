9a184f47500dbe271f580479da191fae61cc2ebd
kashyapanshu3108-ops <kashyapanshu3106@gmail.com>
2026-08-11 23:53:32 +0530

    add responsen in dictionary

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ print("you can ask me basic question, type 'bye'to exit from the bot")
 
 # chatbot memory creation [dictionaey of responce]
 responses={
-  "hello":"hi,welcome. how can i help you?"
-
+  "hello":"hi,welcome. how can i help you?",
+"how are you ":"i am very fine. thank you",
+"who are you":"i am smart chatbot",
+"motivate me":" keep going. every bug of your project make you brtter developes",
+"happy":"happy to hear that",
   }
