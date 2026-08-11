b1e3ce419726e38d4c60ae1fbc8b93f9538a26ae
kashyapanshu3108-ops <kashyapanshu3106@gmail.com>
2026-08-11 23:41:28 +0530

    add dictionary

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 print("HELLLO! WELCOME TO THE  CHATBOT ")
 print("you can ask me basic question, type 'bye'to exit from the bot")
 
+# chatbot memory creation [dictionaey of responce]
+responses={
+  "hello":"hi,welcome. how can i help y"
+
+  }
